92653e6cf686c836c434e006d933b91a4fdfafd6
brnydr <brianwilliamyoder@gmail.com>
2023-10-19 12:08:03 -0700

    add machine model

---
+++ Factory/Models/Machine.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using AspNetCore;
+
+namespace Factory.Models
+{
+  public class Machines
+  {
+    public int MachineId { get; set; }
+    [Required(ErrorMessage = "Please enter a name for the machine.")]
+    public string Model { get; set; }
+    public List<EngineerMachine> JoinEntities { get; set; }
+  }
+}
