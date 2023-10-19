10324ec6a703f3a087121c77f43a220512ba958d
brnydr <brianwilliamyoder@gmail.com>
2023-10-19 12:05:28 -0700

    add engineer model

---
+++ Factory/Models/Engineer.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+namespace Factory.Models
+{
+  public class Engineer
+  {
+    public int EngineerId {get; set;}
+    [Required(ErrorMessage = "Please enter a name for this engineer.")]
+    public string Name {get; set;}
+    public List<EngineerMachine> JoinEntities {get; set;}
+  }
+}
