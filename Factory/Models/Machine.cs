04e7f9d0560d3f163742f0a73c3b788472ef13dc
brnydr <brianwilliamyoder@gmail.com>
2023-10-19 20:59:01 -0700

    refactor model

--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -7,7 +7,7 @@ namespace Factory.Models
   public class Machine
   {
     public int MachineId { get; set; }
-    [Required(ErrorMessage = "Please enter a name for the machine.")]
+    [Required]
     public string Model { get; set; }
     public List<EngineerMachine> JoinEntities { get; set; }
   }
