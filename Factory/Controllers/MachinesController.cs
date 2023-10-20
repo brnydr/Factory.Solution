177ac6b01d44d631181893c4dedb46d2cbb7ef24
brnydr <brianwilliamyoder@gmail.com>
2023-10-20 14:55:34 -0700

    add machine index view and route

---
+++ Factory/Controllers/MachinesController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Linq;
+using Factory.Models;
+
+namespace Factory.Controllers
+{
+  public class MachinesController : Controller
+  {
+    private readonly FactoryContext _db;
+
+    public MachinesController(FactoryContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      ViewBag.PageTitle = "Machines";
+      return View(_db.Machines.ToList());
+    }
+  }
+}
